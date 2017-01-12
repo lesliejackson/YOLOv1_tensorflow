2bef6e6232a0065804a33b7b8034f568e3ba934d
CloudYang <yangyun15@lzu.edu.cn>
2017-01-12 11:04:39 +0800

    Update tiny_yolo.py

--- tiny_yolo.py
+++ tiny_yolo.py
@@ -25,15 +25,19 @@ B = 2
 CLASSES = 2
 COORD_W = 5
 NOOBJ_W = 0.5
-TRAIN_IMG_DIR = '/home/yy/train/'
-TRAIN_LABEL_DIR = '/home/yy/labels/'
 PROB_THRESHOLD = 0.5
 NMS_THRESHOLD = 0.5
-CLASSES_NAME = ["DaLai","NonDaLai"]
 TRAIN_SIZE = 122
 alpha = 0.1
-TEST_IMG_PATH = '/home/yy/109.jpg'
 EVAL_FREQUENCY = 100
+TRAIN_IMG_DIR = '/home/yy/train/'
+TRAIN_LABEL_DIR = '/home/yy/labels/'
+CLASSES_NAME = ["DaLai","NonDaLai"]
+TEST_IMG_PATH = '/home/yy/109.jpg'
+RES_DIR = '/home/yy/pred_shuffle/'
+SAVE_MODEL = '/home/yy/tf_saver_models/model_newls.ckpt'
+SAVE_TENSORBOARD = '/home/yy/tensorboard'
+TEST_MODEL = '/home/yy/tf_saver_models/model_newls.ckpt'
 
 
 conv1_weights = tf.Variable(tf.truncated_normal([3, 3, NUM_CHANNELS, 16], stddev=0.1, seed=SEED, dtype=tf.float32))
@@ -170,7 +174,7 @@ def get_results(output):
 
       cls_ind = probs.argsort()[::-1][0]
       if probs[cls_ind] > PROB_THRESHOLD:
-        results.append([output[0][j-4] - output[0][j-2]/2, output[0][j-3] - output[0][j-3]/2, output[0][j-4] + output[0][j-2]/2, output[0][j-3] + output[0][j-3]/2, output[0][j]])
+        results.append([output[0][j-4] - output[0][j-2]/2, output[0][j-3] - output[0][j-3]/2, output[0][j-4] + output[0][j-2]/2, output[0][j-3] + output[0][j-3]/2, probs[cls_ind]])
         classes.append(cls_ind)
 
   res = numpy.array(results).astype(numpy.float32)
@@ -198,7 +202,7 @@ def show_results(img_path, results, classes):
       cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
       cv2.putText(img, CLASSES_NAME[classes[i]] + ' : %.2f' % results[i][4], (x1+5,y1-7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
 
-  cv2.imwrite('/home/yy/prediction/' + img_path.split('/')[-1], img)
+  cv2.imwrite(RES_DIR + img_path.split('/')[-1], img)
 
 def get_next_minibatch(offset, path_list):
   if offset+BATCH_SIZE > len(path_list):
@@ -267,16 +271,16 @@ def loss_func_yolo(output, exp):
   for i in range(BATCH_SIZE):
     for j in range(0, S*S*(B*5+CLASSES), B*5+CLASSES):
       res += COORD_W * tf.sign(exp[i][j+2]) * (tf.square(output[i][j] - exp[i][j]) + tf.square(output[i][j+1]-exp[i][j+1]) + 
-                                               tf.square(output[i][j+2] - exp[i][j+2]) + 
-                                               tf.square(output[i][j+3] - exp[i][j+3]))
+                                               tf.square(tf.sqrt(tf.abs(output[i][j+2])) - tf.sqrt(exp[i][j+2])) + 
+                                               tf.square(tf.sqrt(tf.abs(output[i][j+3])) - tf.sqrt(exp[i][j+3])))
 
       res += tf.sign(exp[i][j+2]) * (tf.square(output[i][j+4] - exp[i][j+4]))
 
       res += NOOBJ_W * tf.sign(tf.floor(exp[i][j])) * (tf.square(output[i][j+4] - exp[i][j+4]))
 
       res += COORD_W * tf.sign(exp[i][j+7]) * (tf.square(output[i][j+5] - exp[i][j+5]) + tf.square(output[i][j+6]-exp[i][j+6]) + 
-                                               tf.square(output[i][j+7] - exp[i][j+7]) + 
-                                               tf.square(output[i][j+8] - exp[i][j+8]))
+                                               tf.square(tf.sqrt(tf.abs(output[i][j+7])) - tf.sqrt(exp[i][j+7])) + 
+                                               tf.square(tf.sqrt(tf.abs(output[i][j+8])) - tf.sqrt(exp[i][j+8])))
 
       res += tf.sign(exp[i][j+7]) * (tf.square(output[i][j+9] - exp[i][j+9]))
 
@@ -290,10 +294,9 @@ def test(img):
   with tf.Session() as sess:
     tf.global_variables_initializer().run()
     saver = tf.train.Saver()
-    saver.restore(sess, '/home/yy/tf_saver_models/model_448.ckpt')
+    saver.restore(sess, TEST_MODEL)
     data = extract_data_yolo(img, False)
-    out = model(data)
-    out = sess.run(out)
+    out = sess.run(model(data))
     results,classes = get_results(out)
     show_results(img, results, classes)
 
@@ -340,7 +343,7 @@ def main(argv=None):
     tf.global_variables_initializer().run()
     saver = tf.train.Saver()
     print('Initialized!')
-    writer = tf.summary.FileWriter('/home/yy/tensorboard', sess.graph)
+    writer = tf.summary.FileWriter(SAVE_TENSORBOARD, sess.graph)
 
     for step in xrange(int(num_epochs * TRAIN_SIZE) // BATCH_SIZE):
       offset = (step * BATCH_SIZE) % (TRAIN_SIZE - BATCH_SIZE)
@@ -355,7 +358,7 @@ def main(argv=None):
       if step % EVAL_FREQUENCY == 0:
         print('loss: %.6f' % los)
         writer.add_summary(summary, step)
-    save_path = saver.save(sess, "/home/yy/tf_saver_models/model_448.ckpt")
+    save_path = saver.save(sess, SAVE_MODEL)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='YOLO demo')
